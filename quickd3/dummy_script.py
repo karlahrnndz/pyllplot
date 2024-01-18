095b891e48b02969a2217d920f79c345ebac2776
Karla Hernandez <karlahrnndz.dev@gmail.com>
2024-01-17 17:42:23 -0800

    Partial debugging (up to but not includding lb and ub creation)

--- quickd3/dummy_script.py
+++ quickd3/dummy_script.py
@@ -1,7 +1,17 @@
+import pandas as pd
+
 from quickd3 import StackOrderD3Plot
 
-data = {"example": [1, 2, 3]}
+data = pd.DataFrame(
+    {
+        "my_x": [1, 2, 3, 1, 2, 3],
+        "my_y": [3, 2, 1, 1, 2, 1],
+        "my_label": ["a", "a", "a", "b", "b", "b"],
+    }
+)
 
-stackorder_plot = StackOrderD3Plot(data)
+stackorder_plot = StackOrderD3Plot(
+    data, x_col="my_x", y_col="my_y", label_col="my_label"
+)
 stackorder_output = stackorder_plot.plot()
 print(f"StackOrder Output: {stackorder_output}")
