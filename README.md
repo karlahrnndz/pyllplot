78e3e82bb9cb6df8e8ebf19f12e2f1c694e8061d
Karla Hernández <12849170+karlahrnndz@users.noreply.github.com>
2024-02-07 02:33:33 -0800

    Dev (#1)
    * Package template.
    
    * Added dev requirements.
    
    * black
    
    * added ocal hooks-dir
    
    * update requirements, delete unnecessary hooks.
    
    * draft, will likely redo
    
    * Redesigning project.
    
    * linting
    
    * Delete unnecessary files.
    
    * Partial debugging (up to but not includding lb and ub creation)
    
    * First draft complete, need to debug and test edge cases, and refactor html code.
    
    * Set area opacity to 100%
    
    * Updated sorted stream graph to be made with matplotlib.
    
    * linting and formatting
    
    * Update README.md
    
    * minor refactor
    
    * Explicit mention of kwargs in dummy script.
    
    * Checking pre-commit hook.
    
    * Added title to sorted stream graph.
    
    * Edtended functionality to allow for non numerocal values of x such as string or timestamps.
    
    * Deleted unnecessary file.
    
    * linting and pep8
    
    * fixed issue with sorting when duplicates present
    
    * pep8
    
    * Renamed package.
    
    * Updated package URL in pyproject.toml
    
    * Updated package dependencies.
    
    * Changed implementation of SortedStream to be plotly-based.
    
    * Deleted unneccessacy comment.
    
    * Updated example.
    
    * Updated dev requirements file.
    
    * Updated pyprojecttoml and added MANIFEST.in.
    
    * Moved MANIFEST.in to correct folder.
    
    * Updated pyproject.toml and pre-commit-configs.yaml to avoid duplicating information.
    
    * updated manifest
    
    * updated manifest
    
    * Updated gitignore and p=toml
    
    * updated pyproject.toml to include extra files.
    
    * added kaleido to dependencies
    
    * Added docs template.
    
    * Moved docs to docs folder
    
    * Deleted docs, moving to separate branch
    
    * Moved docs to root of docs folder
    
    * start over
    
    * added sphynx docs
    
    * moved index to root of docs folder
    
    * changed basic.css import.
    
    * changed basic.css import.
    
    * changed basic.css import.
    
    * Deleted docs, will move to new branch
    
    * Update README.md
    
    * Update README.md
    
    Added link to docs.
    
    * Update README.md
    
    Updated docs link.
    
    * Added image for reference in docs.
    
    * Modified pyproject.toml and __init__.py to update version and configure bumpver.
    
    * Configured bumpver

---
+++ README.md
@@ -0,0 +1,1 @@
+Working documentation [here](https://karlahrnndz.github.io/pyllplot/).
---
+++ pyllplot/__init__.py
@@ -0,0 +1,12 @@
+"""
+pyllplot: A Python package for [provide a brief description of the package].
+"""
+
+# Version of the pyllplot package following Semantic Versioning (SemVer)
+__version__ = "2024.2.7.1"
+
+# Import specific modules or classes from subpackages
+from .custom_plots.sorted_stream import SortedStream
+
+# Specify modules or classes to be included when using "from pyllplot import *"
+__all__ = ["SortedStream"]
