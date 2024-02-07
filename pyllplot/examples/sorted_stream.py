aa581e3bcf46d0c27286a6abf75a40db10f48101
Karla Hernandez <karlahrnndz.dev@gmail.com>
2024-02-06 17:44:22 -0800

    Updated example.

--- pyllplot/examples/sorted_stream.py
+++ pyllplot/examples/sorted_stream.py
@@ -1,36 +1,26 @@
 from pyllplot import SortedStream
+import plotly.io as pio
+import pandas as pd
 
-# Example using a dictionary
+# Example where the values in x are datetime objects
 data_dict = {
-    "x": [3, 1, 2, 1, 2, 3],
+    "x": [
+        "2022-01-01",
+        "2022-01-02",
+        "2022-01-03",
+        "2022-01-01",
+        "2022-01-02",
+        "2022-01-03",
+    ],
     "height": [1, 1, 1, 3, 2, 3],
     "label": ["lab_1", "lab_1", "lab_1", "lab_2", "lab_2", "lab_2"],
 }
-# data_dict = {
-#     'x': ['a', 'b', 'c', 'a', 'b', 'c',],
-#     'height': [1, 1, 1, 3, 2, 3],
-#     'label': ['lab_1', 'lab_1', 'lab_1', 'lab_2', 'lab_2', 'lab_2']
-# }
-
-# data_dict = {
-#     'x': ['2022-01-01 12:34:56', '2022-01-02 12:34:56', '2022-02-01 12:36:00',
-#           '2022-01-01 12:34:56', '2022-01-02 12:34:56', '2022-02-01 12:36:00'],
-#     'height': [1, 1, 1, 3, 2, 3],
-#     'label': ['lab_1', 'lab_1', 'lab_1', 'lab_2', 'lab_2', 'lab_2']
-# }
-# data_dict['x'] = pd.to_datetime(data_dict['x'])
-
-# data_dict = {
-#     'x': ['2022-01-01', '2022-01-02', '2022-01-03',
-#           '2022-01-01', '2022-01-02', '2022-01-03',],
-#     'height': [1, 1, 1, 3, 2, 3],
-#     'label': ['lab_1', 'lab_1', 'lab_1', 'lab_2', 'lab_2', 'lab_2']
-# }
-# data_dict['x'] = pd.to_datetime(data_dict['x'])
+data_dict["x"] = pd.to_datetime(data_dict["x"])
 
 custom_plot = SortedStream(
     data=data_dict, pad=0, centered=True, color_dict=None, smooth=True, interp_res=10000
 )
-custom_plot.fig.show()
 
-# print('hi')
+# custom_plot.fig is a plotly.graph_objects object with the final visualization
+custom_plot.fig.show()
+pio.write_image(custom_plot.fig, "sorted_stream.svg")
