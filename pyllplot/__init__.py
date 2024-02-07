360e614d5bc271183aee2d44128ff0374e6ce879
Karla Hernandez <karlahrnndz.dev@gmail.com>
2024-02-06 18:26:37 -0800

    Updated pyprojecttoml and added MANIFEST.in.

--- pyllplot/__init__.py
+++ pyllplot/__init__.py
@@ -1,7 +1,12 @@
-# Version of the pyllplot package
-__version__ = "2024.1.0"
+"""
+pyllplot: A Python package for [provide a brief description of the package].
+"""
 
-# from .custom_plots.base import BasePlot
+# Version of the pyllplot package following Semantic Versioning (SemVer)
+__version__ = "1.0.0"
+
+# Import specific modules or classes from subpackages
 from .custom_plots.sorted_stream import SortedStream
 
+# Specify modules or classes to be included when using "from pyllplot import *"
 __all__ = ["SortedStream"]
