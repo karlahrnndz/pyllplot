510f6b4370d3071f4f05b224ef5486a9203907b7
Karla Hernandez <karlahrnndz.dev@gmail.com>
2024-02-07 02:30:08 -0800

    Configured bumpver

--- pyllplot/__init__.py
+++ pyllplot/__init__.py
@@ -3,7 +3,7 @@ pyllplot: A Python package for [provide a brief description of the package].
 """
 
 # Version of the pyllplot package following Semantic Versioning (SemVer)
-__version__ = "2024.2.7.0"
+__version__ = "2024.2.7.1"
 
 # Import specific modules or classes from subpackages
 from .custom_plots.sorted_stream import SortedStream
